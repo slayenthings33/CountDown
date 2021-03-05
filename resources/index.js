690fa588fe2d0c9f065a49a7092e198fd8b22fd1
Ryan Slayen <ryan_slayen@yahoo.com>
2021-03-05 13:27:11 +0100

    final commit

--- resources/index.js
+++ resources/index.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("July 2, 2020 9:00:00").getTime();
+var countDownDate = new Date("March 12, 2021 12:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -23,6 +23,6 @@ var x = setInterval(function() {
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("timer").innerHTML = "THE TIME HAS COME!!!";
+    document.getElementById("timer").innerHTML = "Get ready for this DICK!";
   }
 }, 1000);
